40e7f367d53236737b79ceec3f79b4068add0034
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-09-06 20:09:12 +0530

    Added support to parse `hours-minutes` and `day-hours-minutes`

--- lib/unitify/composite_measurements/time.rb
+++ lib/unitify/composite_measurements/time.rb
@@ -10,6 +10,8 @@ module Unitify
       class << self
         def parse(string)
           case string
+          when D_H_MIN_REGEX  then parse_d_h_min(string)
+          when H_MIN_REGEX    then parse_h_min(string)
           when DURATION_REGEX then parse_duration(string)
           else                     raise Unitify::ParseError, string
           end
@@ -26,11 +28,35 @@ module Unitify
             Unitify::Time.new((seconds || 0), :s) +
             Unitify::Time.new((microseconds || 0), :μs)
         end
+
+        def parse_d_h_min(string)
+          days, hours, minutes = string.match(D_H_MIN_REGEX)&.captures
+
+          if days && hours && minutes
+            Unitify::Time.new(days, :d) +
+              Unitify::Time.new(hours, :h) +
+              Unitify::Time.new(minutes, :min)
+          end
+        end
+
+        def parse_h_min(string)
+          hours, minutes = string.match(H_MIN_REGEX)&.captures
+
+          if hours && minutes
+            Unitify::Time.new(hours, :h) + Unitify::Time.new(minutes, :min)
+          end
+        end
       end
 
       private
 
+      H_REGEX        = /(?:h|hr|hour(?:s)?)/.freeze
+      MIN_REGEX      = /(?:min|minute(?:s)?)/.freeze
+      D_REGEX        = /(?:d|day(?:s)?)/.freeze
+
       DURATION_REGEX = /\A(?<hour>#{ANY_DIGIT}):(?<min>#{ANY_DIGIT}):(?:(?<sec>#{ANY_DIGIT}))?(?:,(?<msec>#{ANY_DIGIT}))?\z/.freeze
+      H_MIN_REGEX    = /\A#{NUMBER_WITH_T_SPACES}#{H_REGEX}#{NUMBER_WITH_LT_SPACES}#{MIN_REGEX}\z/.freeze
+      D_H_MIN_REGEX  = /\A#{NUMBER_WITH_T_SPACES}#{D_REGEX}#{NUMBER_WITH_LT_SPACES}#{H_REGEX}#{NUMBER_WITH_LT_SPACES}#{MIN_REGEX}\z/.freeze
     end
   end
 end
--- spec/unitify/composite_measurements/time_spec.rb
+++ spec/unitify/composite_measurements/time_spec.rb
@@ -13,6 +13,20 @@ RSpec.describe Unitify::CompositeMeasurements::Time do
         expect(subject.parse("12:60:60,60").to_s).to eq("13.016666683333334 h")
         expect(subject.parse("12:60:60").to_s).to eq("13.016666666666667 h")
       end
+
+      it "parses hours and minutes" do
+        expect(subject.parse("3 h 45 min").to_s).to eq("3.75 h")
+        expect(subject.parse("3 hr 45 min").to_s).to eq("3.75 h")
+        expect(subject.parse("3 hour 45 minute").to_s).to eq("3.75 h")
+        expect(subject.parse("3 hours 45 minutes").to_s).to eq("3.75 h")
+      end
+
+      it "parses days, hours, and minutes" do
+        expect(subject.parse("7 d 12 h 15 min").to_s).to eq("7.510416666666667 d")
+        expect(subject.parse("7 d 12 hr 15 min").to_s).to eq("7.510416666666667 d")
+        expect(subject.parse("7 day 12 hour 15 minute").to_s).to eq("7.510416666666667 d")
+        expect(subject.parse("7 days 12 hours 15 minutes").to_s).to eq("7.510416666666667 d")
+      end
     end
 
     context "when invalid string is passed" do
@@ -20,6 +34,14 @@ RSpec.describe Unitify::CompositeMeasurements::Time do
         expect { subject.parse("10:20") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("10:20,20:30").to_s }.to raise_error(Unitify::ParseError)
         expect { subject.parse("10:20,20,30").to_s }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("3 hs 45 mins") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("3 hourz 45 minutez") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("3 hou 45 minut") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("7 dz 12 hz 15 minz") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("7 dayz 12 hourz 15 minutez") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("7 d 12 hou 15 minue") }.to raise_error(Unitify::ParseError)
       end
     end
   end
