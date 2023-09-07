660a77fd5e9c2d92527ea6912990cf49f4848a92
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-09-07 20:16:52 +0530

    Added support to parse `tonnes-kilogrammes` and `stones-pounds-ounces`

--- lib/unitify/composite_measurements/weight.rb
+++ lib/unitify/composite_measurements/weight.rb
@@ -10,10 +10,12 @@ module Unitify
       class << self
         def parse(string)
           case string
-          when POUNDS_OUNCES_REGEX       then parse_pounds_ounces(string)
-          when STONES_POUNDS_REGEX       then parse_stones_pounds(string)
-          when KILOGRAMMES_GRAMMES_REGEX then parse_kilogrammes_grammes(string)
-          else                                raise Unitify::ParseError, string
+          when POUNDS_OUNCES_REGEX        then parse_pounds_ounces(string)
+          when STONES_POUNDS_REGEX        then parse_stones_pounds(string)
+          when STONES_POUNDS_OUNCES_REGEX then parse_stones_pounds_ounces(string)
+          when KILOGRAMMES_GRAMMES_REGEX  then parse_kilogrammes_grammes(string)
+          when TONNES_KILOGRAMMES_REGEX   then parse_tonnes_kilogrammes(string)
+          else                                 raise Unitify::ParseError, string
           end
         end
 
@@ -42,6 +44,24 @@ module Unitify
             Unitify::Weight.new(kilogrammes, :kg) + Unitify::Weight.new(grammes, :g)
           end
         end
+
+        def parse_tonnes_kilogrammes(string)
+          tonnes, kilogrammes = string.match(TONNES_KILOGRAMMES_REGEX)&.captures
+
+          if tonnes && kilogrammes
+            Unitify::Weight.new(tonnes, :t) + Unitify::Weight.new(kilogrammes, :kg)
+          end
+        end
+
+        def parse_stones_pounds_ounces(string)
+          stones, pounds, ounces = string.match(STONES_POUNDS_OUNCES_REGEX)&.captures
+
+          if stones && pounds && ounces
+            Unitify::Weight.new(stones, :st) +
+              Unitify::Weight.new(pounds, :lb) +
+              Unitify::Weight.new(ounces, :oz)
+          end
+        end
       end
 
       private
@@ -51,10 +71,13 @@ module Unitify
       STONES_UNIT_REGEX      = /(?:st|stone(?:s)?)/.freeze
       GRAMMES_UNIT_REGEX     = /(?:g|gram(?:s)?|gramme(?:s)?)/.freeze
       KILOGRAMMES_UNIT_REGEX = /(?:kg|kilogram(?:s)?|kilogramme(?:s)?)/.freeze
+      TONNES_UNIT_REGEX      = /(?:t|tonne(?:s)?|metric tonne(?:s)?)/.freeze
 
-      POUNDS_OUNCES_REGEX       = /\A#{NUMBER_WITH_T_SPACES}#{POUNDS_UNIT_REGEX}#{NUMBER_WITH_LT_SPACES}#{OUNCES_UNIT_REGEX}\z/.freeze
-      STONES_POUNDS_REGEX       = /\A#{NUMBER_WITH_T_SPACES}#{STONES_UNIT_REGEX}#{NUMBER_WITH_LT_SPACES}#{POUNDS_UNIT_REGEX}\z/.freeze
-      KILOGRAMMES_GRAMMES_REGEX = /\A#{NUMBER_WITH_T_SPACES}#{KILOGRAMMES_UNIT_REGEX}#{NUMBER_WITH_LT_SPACES}#{GRAMMES_UNIT_REGEX}\z/.freeze
+      POUNDS_OUNCES_REGEX        = /\A#{NUMBER_WITH_T_SPACES}#{POUNDS_UNIT_REGEX}#{NUMBER_WITH_LT_SPACES}#{OUNCES_UNIT_REGEX}\z/.freeze
+      STONES_POUNDS_REGEX        = /\A#{NUMBER_WITH_T_SPACES}#{STONES_UNIT_REGEX}#{NUMBER_WITH_LT_SPACES}#{POUNDS_UNIT_REGEX}\z/.freeze
+      KILOGRAMMES_GRAMMES_REGEX  = /\A#{NUMBER_WITH_T_SPACES}#{KILOGRAMMES_UNIT_REGEX}#{NUMBER_WITH_LT_SPACES}#{GRAMMES_UNIT_REGEX}\z/.freeze
+      STONES_POUNDS_OUNCES_REGEX = /\A#{NUMBER_WITH_T_SPACES}#{STONES_UNIT_REGEX}#{NUMBER_WITH_LT_SPACES}#{POUNDS_UNIT_REGEX}#{NUMBER_WITH_LT_SPACES}#{OUNCES_UNIT_REGEX}\z/.freeze
+      TONNES_KILOGRAMMES_REGEX   = /\A#{NUMBER_WITH_T_SPACES}#{TONNES_UNIT_REGEX}#{NUMBER_WITH_LT_SPACES}#{KILOGRAMMES_UNIT_REGEX}\z/.freeze
     end
   end
 end
--- spec/unitify/composite_measurements/weight_spec.rb
+++ spec/unitify/composite_measurements/weight_spec.rb
@@ -28,12 +28,30 @@ RSpec.describe Unitify::CompositeMeasurements::Weight do
         expect(subject.parse("2 stones 6 pounds").to_s).to eq("2.428571428571429 st")
       end
 
+      it "parses stones, pounds, and ounces" do
+        expect(subject.parse("14 st 2 lb 5 oz").to_s).to eq("14.165178571428571 st")
+        expect(subject.parse("14 st 2 lbs 5 oz").to_s).to eq("14.165178571428571 st")
+        expect(subject.parse("14 st 2 lbm 5 oz").to_s).to eq("14.165178571428571 st")
+        expect(subject.parse("14 st 2 # 5 oz").to_s).to eq("14.165178571428571 st")
+        expect(subject.parse("14 st 2 pound-mass 5 oz").to_s).to eq("14.165178571428571 st")
+        expect(subject.parse("14 stone 2 pound 5 ounce").to_s).to eq("14.165178571428571 st")
+        expect(subject.parse("14 stones 2 pounds 5 ounces").to_s).to eq("14.165178571428571 st")
+      end
+
       it "parses kilogrammes and grammes" do
         expect(subject.parse("4 kg 500 g").to_s).to eq("4.5 kg")
         expect(subject.parse("4 kilogramme 500 gramme").to_s).to eq("4.5 kg")
         expect(subject.parse("4 kilogrammes 500 grammes").to_s).to eq("4.5 kg")
         expect(subject.parse("4 kilogram 500 gram").to_s).to eq("4.5 kg")
         expect(subject.parse("4 kilograms 500 grams").to_s).to eq("4.5 kg")
+      end
+
+      it "parses tonnes and kilogrammes" do
+        expect(subject.parse("1 t 500 kg").to_s).to eq("1.5 t")
+        expect(subject.parse("1 tonne 500 kilogram").to_s).to eq("1.5 t")
+        expect(subject.parse("1 tonnes 500 kilograms").to_s).to eq("1.5 t")
+        expect(subject.parse("1 metric tonne 500 kilogramme").to_s).to eq("1.5 t")
+        expect(subject.parse("1 metric tonnes 500 kilogrammes").to_s).to eq("1.5 t")
       end
     end
 
@@ -52,6 +70,14 @@ RSpec.describe Unitify::CompositeMeasurements::Weight do
         expect { subject.parse("2 st 6 pound mass") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("2 st 6 pound mass") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("2 st 6 ##") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("14 sts 2 lbs 5 ozs") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("14 stz 2 lbz 5 ozz") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("14 stoness 2 poundss 5 ouncess") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("1 ts 500 kgs") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("1 tonness 500 kilogramss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("1 metric tonness 500 kilogrammess") }.to raise_error(Unitify::ParseError)
 
         expect { subject.parse("4 kgs 500 gs") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("4 kilogramz 500 gramz") }.to raise_error(Unitify::ParseError)
