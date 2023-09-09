ba640fd6cda6a832b99975dade175c0453d9eb83
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-09-09 18:36:58 +0530

    Added new measurements parsing capabilities in `time`

--- lib/unitify/composite_measurements/time.rb
+++ lib/unitify/composite_measurements/time.rb
@@ -10,16 +10,21 @@ module Unitify
       class << self
         def parse(string)
           case string
-          when HOUR_MINUTE     then parse_hour_minute(string)
-          when MINUTE_SECOND   then parse_minute_second(string)
-          when WEEK_DAY        then parse_week_day(string)
-          when MONTH_DAY       then parse_month_day(string)
-          when YEAR_MONTH      then parse_year_month(string)
-          when QUARTER_MONTH   then parse_quarter_month(string)
-          when FORTNIGHT_DAY   then parse_fortnight_day(string)
-          when DURATION        then parse_duration(string)
-          when DAY_HOUR_MINUTE then parse_day_hour_minute(string)
-          else                      raise Unitify::ParseError, string
+          when HOUR_MINUTE          then parse_hour_minute(string)
+          when MINUTE_SECOND        then parse_minute_second(string)
+          when SECONDS_MILLISECONDS then parse_second_millisecond(string)
+          when WEEK_DAY             then parse_week_day(string)
+          when MONTH_DAY            then parse_month_day(string)
+          when YEAR_MONTH           then parse_year_month(string)
+          when QUARTER_MONTH        then parse_quarter_month(string)
+          when FORTNIGHT_DAY        then parse_fortnight_day(string)
+          when WEEK_DAY_HOUR        then parse_week_day_hour(string)
+          when MONTH_WEEK_DAY       then parse_month_week_day(string)
+          when DAY_HOUR_MINUTE      then parse_day_hour_minute(string)
+          when YEAR_MONTH_DAY       then parse_year_month_day(string)
+          when HOUR_MINUTE_SECOND   then hour_minute_second(string)
+          when DURATION             then parse_duration(string)
+          else                           raise Unitify::ParseError, string
           end
         end
 
@@ -38,6 +43,14 @@ module Unitify
 
           if minute && second
             Unitify::Time.new(minute, :min) + Unitify::Time.new(second, :s)
+          end
+        end
+
+        def parse_second_millisecond(string)
+          second, millisecond = string.match(SECONDS_MILLISECONDS)&.captures
+
+          if second && millisecond
+            Unitify::Time.new(second, :s) + Unitify::Time.new(millisecond, :ms)
           end
         end
 
@@ -81,14 +94,24 @@ module Unitify
           end
         end
 
-        def parse_duration(string)
-          hour, minute, second, microsecond = string.match(DURATION)&.captures
-          raise ArgumentError, "Invalid Duration" if [hour, minute, second, microsecond].all?(&:nil?)
+        def parse_week_day_hour(string)
+          week, day, hour = string.match(WEEK_DAY_HOUR)&.captures
 
-          Unitify::Time.new((hour || 0), :h) +
-            Unitify::Time.new((minute || 0), :min) +
-            Unitify::Time.new((second || 0), :s) +
-            Unitify::Time.new((microsecond || 0), :μs)
+          if week && day && hour
+            Unitify::Time.new(week, :wk) +
+            Unitify::Time.new(day, :d) +
+              Unitify::Time.new(hour, :h)
+          end
+        end
+
+        def parse_month_week_day(string)
+          month, week, day = string.match(MONTH_WEEK_DAY)&.captures
+
+          if month && week && day
+            Unitify::Time.new(month, :mo) +
+              Unitify::Time.new(week, :wk) +
+              Unitify::Time.new(day, :d)
+          end
         end
 
         def parse_day_hour_minute(string)
@@ -100,29 +123,65 @@ module Unitify
               Unitify::Time.new(minute, :min)
           end
         end
+
+        def parse_year_month_day(string)
+          year, month, day = string.match(YEAR_MONTH_DAY)&.captures
+
+          if year && month && day
+            Unitify::Time.new(year, :yr) +
+              Unitify::Time.new(month, :mo) +
+              Unitify::Time.new(day, :d)
+          end
+        end
+
+        def hour_minute_second(string)
+          hour, minute, second = string.match(HOUR_MINUTE_SECOND)&.captures
+
+          if hour && minute && second
+            Unitify::Time.new(hour, :h) +
+              Unitify::Time.new(minute, :min) +
+              Unitify::Time.new(second, :s)
+          end
+        end
+
+        def parse_duration(string)
+          hour, minute, second, microsecond = string.match(DURATION)&.captures
+          raise ArgumentError, "Invalid Duration" if [hour, minute, second, microsecond].all?(&:nil?)
+
+          Unitify::Time.new((hour || 0), :h) +
+            Unitify::Time.new((minute || 0), :min) +
+            Unitify::Time.new((second || 0), :s) +
+            Unitify::Time.new((microsecond || 0), :μs)
+        end
       end
 
       private
 
-      HOUR_UNITS      = /(?:h|hr|hour(?:s)?)/.freeze
-      MINUTE_UNITS    = /(?:min|minute(?:s)?)/.freeze
-      SECOND_UNITS    = /(?:s|sec|second(?:s)?)/.freeze
-      DAY_UNITS       = /(?:d|day(?:s)?)/.freeze
-      WEEK_UNITS      = /(?:wk|week(?:s)?)/.freeze
-      MONTH_UNITS     = /(?:mo|month(?:s)?)/.freeze
-      QUARTER_UNITS   = /(?:qtr|quarter(?:s)?)/.freeze
-      YEAR_UNITS      = /(?:y|yr|year(?:s)?)/.freeze
-      FORTNIGHT_UNITS = /(?:fn|4tnite|fortnight(?:s)?)/.freeze
+      HOUR_UNITS        = /(?:h|hr|hour(?:s)?)/.freeze
+      MINUTE_UNITS      = /(?:min|minute(?:s)?)/.freeze
+      SECOND_UNITS      = /(?:s|sec|second(?:s)?)/.freeze
+      DAY_UNITS         = /(?:d|day(?:s)?)/.freeze
+      WEEK_UNITS        = /(?:wk|week(?:s)?)/.freeze
+      MONTH_UNITS       = /(?:mo|month(?:s)?)/.freeze
+      QUARTER_UNITS     = /(?:qtr|quarter(?:s)?)/.freeze
+      YEAR_UNITS        = /(?:y|yr|year(?:s)?)/.freeze
+      FORTNIGHT_UNITS   = /(?:fn|4tnite|fortnight(?:s)?)/.freeze
+      MILLISECOND_UNITS = /(?:ms|millisec|millisecond(?:s)?)/.freeze
 
-      HOUR_MINUTE     = /\A#{NUMBER_WITH_T_SPACES}#{HOUR_UNITS}#{NUMBER_WITH_LT_SPACES}#{MINUTE_UNITS}\z/.freeze
-      MINUTE_SECOND   = /\A#{NUMBER_WITH_T_SPACES}#{MINUTE_UNITS}#{NUMBER_WITH_LT_SPACES}#{SECOND_UNITS}\z/.freeze
-      WEEK_DAY        = /\A#{NUMBER_WITH_T_SPACES}#{WEEK_UNITS}#{NUMBER_WITH_LT_SPACES}#{DAY_UNITS}\z/.freeze
-      MONTH_DAY       = /\A#{NUMBER_WITH_T_SPACES}#{MONTH_UNITS}#{NUMBER_WITH_LT_SPACES}#{DAY_UNITS}\z/.freeze
-      YEAR_MONTH      = /\A#{NUMBER_WITH_T_SPACES}#{YEAR_UNITS}#{NUMBER_WITH_LT_SPACES}#{MONTH_UNITS}\z/.freeze
-      QUARTER_MONTH   = /\A#{NUMBER_WITH_T_SPACES}#{QUARTER_UNITS}#{NUMBER_WITH_LT_SPACES}#{MONTH_UNITS}\z/.freeze
-      FORTNIGHT_DAY   = /\A#{NUMBER_WITH_T_SPACES}#{FORTNIGHT_UNITS}#{NUMBER_WITH_LT_SPACES}#{DAY_UNITS}\z/.freeze
-      DURATION        = /\A(?<hour>#{ANY_REAL_NUMBER}):(?<min>#{ANY_REAL_NUMBER}):(?:(?<sec>#{ANY_REAL_NUMBER}))?(?:,(?<msec>#{ANY_REAL_NUMBER}))?\z/.freeze
-      DAY_HOUR_MINUTE = /\A#{NUMBER_WITH_T_SPACES}#{DAY_UNITS}#{NUMBER_WITH_LT_SPACES}#{HOUR_UNITS}#{NUMBER_WITH_LT_SPACES}#{MINUTE_UNITS}\z/.freeze
+      HOUR_MINUTE          = /\A#{NUMBER_WITH_T_SPACES}#{HOUR_UNITS}#{NUMBER_WITH_LT_SPACES}#{MINUTE_UNITS}\z/.freeze
+      MINUTE_SECOND        = /\A#{NUMBER_WITH_T_SPACES}#{MINUTE_UNITS}#{NUMBER_WITH_LT_SPACES}#{SECOND_UNITS}\z/.freeze
+      WEEK_DAY             = /\A#{NUMBER_WITH_T_SPACES}#{WEEK_UNITS}#{NUMBER_WITH_LT_SPACES}#{DAY_UNITS}\z/.freeze
+      MONTH_DAY            = /\A#{NUMBER_WITH_T_SPACES}#{MONTH_UNITS}#{NUMBER_WITH_LT_SPACES}#{DAY_UNITS}\z/.freeze
+      YEAR_MONTH           = /\A#{NUMBER_WITH_T_SPACES}#{YEAR_UNITS}#{NUMBER_WITH_LT_SPACES}#{MONTH_UNITS}\z/.freeze
+      QUARTER_MONTH        = /\A#{NUMBER_WITH_T_SPACES}#{QUARTER_UNITS}#{NUMBER_WITH_LT_SPACES}#{MONTH_UNITS}\z/.freeze
+      FORTNIGHT_DAY        = /\A#{NUMBER_WITH_T_SPACES}#{FORTNIGHT_UNITS}#{NUMBER_WITH_LT_SPACES}#{DAY_UNITS}\z/.freeze
+      SECONDS_MILLISECONDS = /\A#{NUMBER_WITH_T_SPACES}#{SECOND_UNITS}#{NUMBER_WITH_LT_SPACES}#{MILLISECOND_UNITS}\z/.freeze
+      DAY_HOUR_MINUTE      = /\A#{NUMBER_WITH_T_SPACES}#{DAY_UNITS}#{NUMBER_WITH_LT_SPACES}#{HOUR_UNITS}#{NUMBER_WITH_LT_SPACES}#{MINUTE_UNITS}\z/.freeze
+      WEEK_DAY_HOUR        = /\A#{NUMBER_WITH_T_SPACES}#{WEEK_UNITS}#{NUMBER_WITH_LT_SPACES}#{DAY_UNITS}#{NUMBER_WITH_LT_SPACES}#{HOUR_UNITS}\z/.freeze
+      MONTH_WEEK_DAY       = /\A#{NUMBER_WITH_T_SPACES}#{MONTH_UNITS}#{NUMBER_WITH_LT_SPACES}#{WEEK_UNITS}#{NUMBER_WITH_LT_SPACES}#{DAY_UNITS}\z/.freeze
+      YEAR_MONTH_DAY       = /\A#{NUMBER_WITH_T_SPACES}#{YEAR_UNITS}#{NUMBER_WITH_LT_SPACES}#{MONTH_UNITS}#{NUMBER_WITH_LT_SPACES}#{DAY_UNITS}\z/.freeze
+      HOUR_MINUTE_SECOND   = /\A#{NUMBER_WITH_T_SPACES}#{HOUR_UNITS}#{NUMBER_WITH_LT_SPACES}#{MINUTE_UNITS}#{NUMBER_WITH_LT_SPACES}#{SECOND_UNITS}\z/.freeze
+      DURATION             = /\A(?<hour>#{ANY_REAL_NUMBER}):(?<min>#{ANY_REAL_NUMBER}):(?:(?<sec>#{ANY_REAL_NUMBER}))?(?:,(?<msec>#{ANY_REAL_NUMBER}))?\z/.freeze
     end
   end
 end
--- spec/unitify/composite_measurements/time_spec.rb
+++ spec/unitify/composite_measurements/time_spec.rb
@@ -23,6 +23,13 @@ RSpec.describe Unitify::CompositeMeasurements::Time do
         expect(subject.parse("10 minutes 90 seconds").to_s).to eq("11.5 min")
       end
 
+      it "parses second and millisecond" do
+        expect(subject.parse("8 s 500 ms").to_s).to eq("8.5 s")
+        expect(subject.parse("8 sec 500 millisec").to_s).to eq("8.5 s")
+        expect(subject.parse("8 second 500 millisecond").to_s).to eq("8.5 s")
+        expect(subject.parse("8 seconds 500 milliseconds").to_s).to eq("8.5 s")
+      end
+
       it "parses week and day" do
         expect(subject.parse("8 wk 3 d").to_s).to eq("8.428571428571429 wk")
         expect(subject.parse("8 week 3 day").to_s).to eq("8.428571428571429 wk")
@@ -55,9 +62,17 @@ RSpec.describe Unitify::CompositeMeasurements::Time do
         expect(subject.parse("3 quarters 3 months").to_s).to eq("4 qtr")
       end
 
-      it "parses duration" do
-        expect(subject.parse("12:60:60,60").to_s).to eq("13.016666683333334 h")
-        expect(subject.parse("12:60:60").to_s).to eq("13.016666666666667 h")
+      it "parses week, day, and hour" do
+        expect(subject.parse("2 wk 3 d 6 h").to_s).to eq("2.464285714285715 wk")
+        expect(subject.parse("2 wk 3 d 6 hr").to_s).to eq("2.464285714285715 wk")
+        expect(subject.parse("2 week 3 day 6 hour").to_s).to eq("2.464285714285715 wk")
+        expect(subject.parse("2 weeks 3 days 6 hours").to_s).to eq("2.464285714285715 wk")
+      end
+
+      it "parses month, week, and day" do
+        expect(subject.parse("6 mo 2 wk 4 d").to_s).to eq("6.591780173391591 mo")
+        expect(subject.parse("6 month 2 week 4 day").to_s).to eq("6.591780173391591 mo")
+        expect(subject.parse("6 months 2 weeks 4 days").to_s).to eq("6.591780173391591 mo")
       end
 
       it "parses day, hour, and minute" do
@@ -65,6 +80,25 @@ RSpec.describe Unitify::CompositeMeasurements::Time do
         expect(subject.parse("7 d 12 hr 15 min").to_s).to eq("7.510416666666667 d")
         expect(subject.parse("7 day 12 hour 15 minute").to_s).to eq("7.510416666666667 d")
         expect(subject.parse("7 days 12 hours 15 minutes").to_s).to eq("7.510416666666667 d")
+      end
+
+      it "parses year, month, and day" do
+        expect(subject.parse("15 y 9 mo 20 d").to_s).to eq("15.804794460499222 yr")
+        expect(subject.parse("15 yr 9 mo 20 d").to_s).to eq("15.804794460499222 yr")
+        expect(subject.parse("15 year 9 month 20 day").to_s).to eq("15.804794460499222 yr")
+        expect(subject.parse("15 years 9 months 20 days").to_s).to eq("15.804794460499222 yr")
+      end
+
+      it "parses hour, minute, and second" do
+        expect(subject.parse("2 h 30 min 45 s").to_s).to eq("2.5125 h")
+        expect(subject.parse("2 hr 30 min 45 sec").to_s).to eq("2.5125 h")
+        expect(subject.parse("2 hour 30 minute 45 second").to_s).to eq("2.5125 h")
+        expect(subject.parse("2 hours 30 minutes 45 seconds").to_s).to eq("2.5125 h")
+      end
+
+      it "parses duration" do
+        expect(subject.parse("12:60:60,60").to_s).to eq("13.016666683333334 h")
+        expect(subject.parse("12:60:60").to_s).to eq("13.016666666666667 h")
       end
     end
 
@@ -77,6 +111,10 @@ RSpec.describe Unitify::CompositeMeasurements::Time do
         expect { subject.parse("10 mins 90 s") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("10 minutess 90 secs") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("10 minutez 90 secondz") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("8 ss 500 mss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("8 secondss 500 millisecondss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("8 secondz 500 millisecondz") }.to raise_error(Unitify::ParseError)
 
         expect { subject.parse("8 wks 3 ds") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("8 weekss 3 dayss") }.to raise_error(Unitify::ParseError)
@@ -98,13 +136,29 @@ RSpec.describe Unitify::CompositeMeasurements::Time do
         expect { subject.parse("3 fortnightss 42 dayss") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("3 fortnightz 42 dayz") }.to raise_error(Unitify::ParseError)
 
-        expect { subject.parse("10:20") }.to raise_error(Unitify::ParseError)
-        expect { subject.parse("10:20,20:30").to_s }.to raise_error(Unitify::ParseError)
-        expect { subject.parse("10:20,20,30").to_s }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("2 wkz 3 dz 6 hz") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("2 weekss 3 dayss 6 hourss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("2 weekz 3 dayz 6 hourz") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("6 mos 2 wks 4 ds") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("6 monthss 2 weekss 4 dayss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("6 monthz 2 weekz 4 dayz") }.to raise_error(Unitify::ParseError)
 
         expect { subject.parse("7 dz 12 hz 15 minz") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("7 dayz 12 hourz 15 minutez") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("7 d 12 hou 15 minue") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("15 ys 9 mos 20 ds") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("15 yearss 9 monthss 20 dayss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("15 yearz 9 monthz 20 dayz") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("2 hs 30 mins 45 ss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("2 hourss 30 minutess 45 secondss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("2 hourz 30 minutez 45 secondz") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("10:20") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("10:20,20:30").to_s }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("10:20,20,30").to_s }.to raise_error(Unitify::ParseError)
       end
     end
   end
