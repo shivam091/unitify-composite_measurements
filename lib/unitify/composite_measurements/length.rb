5e3907b882a70e6e918463160a82f6c838f6573d
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-09-09 18:36:49 +0530

    Added new measurements parsing capabilities in `length`

--- lib/unitify/composite_measurements/length.rb
+++ lib/unitify/composite_measurements/length.rb
@@ -10,13 +10,20 @@ module Unitify
       class << self
         def parse(string)
           case string
-          when METRE_CENTIMETRE     then parse_metre_centimetre(string)
-          when FOOT_INCH            then parse_foot_inch(string)
-          when MILE_YARD            then parse_mile_yard(string)
-          when KILOMETRE_METRE      then parse_kilometre_metre(string)
-          when MILE_YARD_FOOT       then parse_mile_yard_foot(string)
-          when FOOT_INCH_CENTIMETRE then parse_foot_inch_centimetre(string)
-          else                           raise Unitify::ParseError, string
+          when METRE_CENTIMETRE           then parse_metre_centimetre(string)
+          when FOOT_INCH                  then parse_foot_inch(string)
+          when MILE_YARD                  then parse_mile_yard(string)
+          when KILOMETRE_METRE            then parse_kilometre_metre(string)
+          when MILE_YARD_FOOT             then parse_mile_yard_foot(string)
+          when FOOT_INCH_YARD             then parse_foot_inch_yard(string)
+          when FOOT_INCH_CENTIMETRE       then parse_foot_inch_centimetre(string)
+          when MILE_FURLONG_YARD          then parse_mile_furlong_yard(string)
+          when CHAIN_FOOT_INCH            then parse_chain_foot_inch(string)
+          when FOOT_INCH_MILLIMETRE       then parse_foot_inch_millimetre(string)
+          when FATHOM_FOOT_INCH           then parse_fathom_foot_inch(string)
+          when FURLONG_CHAIN_FOOT         then parse_furlong_chain_foot(string)
+          when KILOMETRE_METRE_MILLIMETRE then parse_kilometre_metre_millimetre(string)
+          else                                 raise Unitify::ParseError, string
           end
         end
 
@@ -64,6 +71,16 @@ module Unitify
           end
         end
 
+        def parse_foot_inch_yard(string)
+          foot, inch, yard = string.match(FOOT_INCH_YARD)&.captures
+
+          if foot && inch && yard
+            Unitify::Length.new(foot, :ft) +
+              Unitify::Length.new(inch, :in) +
+              Unitify::Length.new(yard, :yd)
+          end
+        end
+
         def parse_foot_inch_centimetre(string)
           foot, inch, centimetre = string.match(FOOT_INCH_CENTIMETRE)&.captures
 
@@ -73,6 +90,66 @@ module Unitify
               Unitify::Length.new(centimetre, :cm)
           end
         end
+
+        def parse_mile_furlong_yard(string)
+          mile, furlong, yard = string.match(MILE_FURLONG_YARD)&.captures
+
+          if mile && furlong && yard
+            Unitify::Length.new(mile, :mi) +
+              Unitify::Length.new(furlong, :fur) +
+              Unitify::Length.new(yard, :yd)
+          end
+        end
+
+        def parse_chain_foot_inch(string)
+          chain, foot, inch = string.match(CHAIN_FOOT_INCH)&.captures
+
+          if chain && foot && inch
+            Unitify::Length.new(chain, :ch) +
+              Unitify::Length.new(foot, :ft) +
+              Unitify::Length.new(inch, :in)
+          end
+        end
+
+        def parse_fathom_foot_inch(string)
+          fathom, foot, inch = string.match(FATHOM_FOOT_INCH)&.captures
+
+          if fathom && foot && inch
+            Unitify::Length.new(fathom, :ftm) +
+              Unitify::Length.new(foot, :ft) +
+              Unitify::Length.new(inch, :in)
+          end
+        end
+
+        def parse_furlong_chain_foot(string)
+          furlong, chain, foot = string.match(FURLONG_CHAIN_FOOT)&.captures
+
+          if furlong && chain && foot
+            Unitify::Length.new(furlong, :fur) +
+              Unitify::Length.new(chain, :ch) +
+              Unitify::Length.new(foot, :ft)
+          end
+        end
+
+        def parse_foot_inch_millimetre(string)
+          foot, inch, millimetre = string.match(FOOT_INCH_MILLIMETRE)&.captures
+
+          if foot && inch && millimetre
+            Unitify::Length.new(foot, :ft) +
+              Unitify::Length.new(inch, :in) +
+              Unitify::Length.new(millimetre, :mm)
+          end
+        end
+
+        def parse_kilometre_metre_millimetre(string)
+          kilometre, metre, millimetre = string.match(KILOMETRE_METRE_MILLIMETRE)&.captures
+
+          if kilometre && metre && millimetre
+            Unitify::Length.new(kilometre, :km) +
+              Unitify::Length.new(metre, :m) +
+              Unitify::Length.new(millimetre, :mm)
+          end
+        end
       end
 
       private
@@ -80,17 +157,28 @@ module Unitify
       FOOT_UNITS       = /(?:'|ft|foot|feet)/.freeze
       INCH_UNITS       = /(?:"|in|inch(?:es)?)/.freeze
       METRE_UNITS      = /(?:m|meter(?:s)?|metre(?:s)?)/.freeze
+      MILLIMETRE_UNITS = /(?:mm|millimeter(?:s)?|millimetre(?:s)?)/.freeze
       CENTIMETRE_UNITS = /(?:cm|centimeter(?:s)?|centimetre(?:s)?)/.freeze
       KILOMETRE_UNITS  = /(?:km|kilometer(?:s)?|kilometre(?:s)?)/.freeze
       MILE_UNITS       = /(?:mi|mile(?:s)?)/.freeze
       YARD_UNITS       = /(?:yd|yard(?:s)?)/.freeze
+      FURLONG_UNITS    = /(?:fur|furlong(?:s)?)/.freeze
+      CHAIN_UNITS      = /(?:ch|chain(?:s)?)/.freeze
+      FATHOM_UNITS     = /(?:ftm|fathom(?:s)?)/.freeze
 
-      FOOT_INCH            = /\A#{NUMBER_WITH_T_SPACES}#{FOOT_UNITS}#{NUMBER_WITH_LT_SPACES}#{INCH_UNITS}\z/.freeze
-      METRE_CENTIMETRE     = /\A#{NUMBER_WITH_T_SPACES}#{METRE_UNITS}#{NUMBER_WITH_LT_SPACES}#{CENTIMETRE_UNITS}\z/.freeze
-      MILE_YARD            = /\A#{NUMBER_WITH_T_SPACES}#{MILE_UNITS}#{NUMBER_WITH_LT_SPACES}#{YARD_UNITS}\z/.freeze
-      KILOMETRE_METRE      = /\A#{NUMBER_WITH_T_SPACES}#{KILOMETRE_UNITS}#{NUMBER_WITH_LT_SPACES}#{METRE_UNITS}\z/.freeze
-      MILE_YARD_FOOT       = /\A#{NUMBER_WITH_T_SPACES}#{MILE_UNITS}#{NUMBER_WITH_LT_SPACES}#{YARD_UNITS}#{NUMBER_WITH_LT_SPACES}#{FOOT_UNITS}\z/.freeze
-      FOOT_INCH_CENTIMETRE = /\A#{NUMBER_WITH_T_SPACES}#{FOOT_UNITS}#{NUMBER_WITH_LT_SPACES}#{INCH_UNITS}#{NUMBER_WITH_LT_SPACES}#{CENTIMETRE_UNITS}\z/.freeze
+      FOOT_INCH                  = /\A#{NUMBER_WITH_T_SPACES}#{FOOT_UNITS}#{NUMBER_WITH_LT_SPACES}#{INCH_UNITS}\z/.freeze
+      METRE_CENTIMETRE           = /\A#{NUMBER_WITH_T_SPACES}#{METRE_UNITS}#{NUMBER_WITH_LT_SPACES}#{CENTIMETRE_UNITS}\z/.freeze
+      MILE_YARD                  = /\A#{NUMBER_WITH_T_SPACES}#{MILE_UNITS}#{NUMBER_WITH_LT_SPACES}#{YARD_UNITS}\z/.freeze
+      KILOMETRE_METRE            = /\A#{NUMBER_WITH_T_SPACES}#{KILOMETRE_UNITS}#{NUMBER_WITH_LT_SPACES}#{METRE_UNITS}\z/.freeze
+      MILE_YARD_FOOT             = /\A#{NUMBER_WITH_T_SPACES}#{MILE_UNITS}#{NUMBER_WITH_LT_SPACES}#{YARD_UNITS}#{NUMBER_WITH_LT_SPACES}#{FOOT_UNITS}\z/.freeze
+      FOOT_INCH_CENTIMETRE       = /\A#{NUMBER_WITH_T_SPACES}#{FOOT_UNITS}#{NUMBER_WITH_LT_SPACES}#{INCH_UNITS}#{NUMBER_WITH_LT_SPACES}#{CENTIMETRE_UNITS}\z/.freeze
+      FOOT_INCH_YARD             = /\A#{NUMBER_WITH_T_SPACES}#{FOOT_UNITS}#{NUMBER_WITH_LT_SPACES}#{INCH_UNITS}#{NUMBER_WITH_LT_SPACES}#{YARD_UNITS}\z/.freeze
+      MILE_FURLONG_YARD          = /\A#{NUMBER_WITH_T_SPACES}#{MILE_UNITS}#{NUMBER_WITH_LT_SPACES}#{FURLONG_UNITS}#{NUMBER_WITH_LT_SPACES}#{YARD_UNITS}\z/.freeze
+      FOOT_INCH_MILLIMETRE       = /\A#{NUMBER_WITH_T_SPACES}#{FOOT_UNITS}#{NUMBER_WITH_LT_SPACES}#{INCH_UNITS}#{NUMBER_WITH_LT_SPACES}#{MILLIMETRE_UNITS}\z/.freeze
+      KILOMETRE_METRE_MILLIMETRE = /\A#{NUMBER_WITH_T_SPACES}#{KILOMETRE_UNITS}#{NUMBER_WITH_LT_SPACES}#{METRE_UNITS}#{NUMBER_WITH_LT_SPACES}#{MILLIMETRE_UNITS}\z/.freeze
+      CHAIN_FOOT_INCH            = /\A#{NUMBER_WITH_T_SPACES}#{CHAIN_UNITS}#{NUMBER_WITH_LT_SPACES}#{FOOT_UNITS}#{NUMBER_WITH_LT_SPACES}#{INCH_UNITS}\z/.freeze
+      FATHOM_FOOT_INCH           = /\A#{NUMBER_WITH_T_SPACES}#{FATHOM_UNITS}#{NUMBER_WITH_LT_SPACES}#{FOOT_UNITS}#{NUMBER_WITH_LT_SPACES}#{INCH_UNITS}\z/.freeze
+      FURLONG_CHAIN_FOOT         = /\A#{NUMBER_WITH_T_SPACES}#{FURLONG_UNITS}#{NUMBER_WITH_LT_SPACES}#{CHAIN_UNITS}#{NUMBER_WITH_LT_SPACES}#{FOOT_UNITS}\z/.freeze
     end
   end
 end
--- spec/unitify/composite_measurements/length_spec.rb
+++ spec/unitify/composite_measurements/length_spec.rb
@@ -53,6 +53,57 @@ RSpec.describe Unitify::CompositeMeasurements::Length do
         expect(subject.parse("9 feet 6 inches 2 centimeters").to_s).to eq("9.565616797900262 ft")
         expect(subject.parse("9 feet 6 inches 2 centimetres").to_s).to eq("9.565616797900262 ft")
       end
+
+      it "parses foot, inch, and yard" do
+        expect(subject.parse("8 ft 10 in 5/16 yd").to_s).to eq("9.770833333333332 ft")
+        expect(subject.parse("8 ' 10 \" 5/16 yd").to_s).to eq("9.770833333333332 ft")
+        expect(subject.parse("8 foot 10 inch 5/16 yard").to_s).to eq("9.770833333333332 ft")
+        expect(subject.parse("8 feet 10 inches 5/16 yards").to_s).to eq("9.770833333333332 ft")
+      end
+
+      it "parses mile, furlong, and yard" do
+        expect(subject.parse("12 mi 3 fur 300 yd").to_s).to eq("12.545454545454545 mi")
+        expect(subject.parse("12 mile 3 furlong 300 yard").to_s).to eq("12.545454545454545 mi")
+        expect(subject.parse("12 miles 3 furlongs 300 yards").to_s).to eq("12.545454545454545 mi")
+      end
+
+      it "parses chain, foot, and inch" do
+        expect(subject.parse("1 ch 66 ft 792 in").to_s).to eq("3 ch")
+        expect(subject.parse("1 ch 66 ' 792 \"").to_s).to eq("3 ch")
+        expect(subject.parse("1 chain 66 foot 792 inch").to_s).to eq("3 ch")
+        expect(subject.parse("1 chains 66 feet 792 inches").to_s).to eq("3 ch")
+      end
+
+      it "parses fathom, foot, and inch" do
+        expect(subject.parse("2 ftm 12 ft 144 in").to_s).to eq("6 ftm")
+        expect(subject.parse("2 ftm 12 ' 144 \"").to_s).to eq("6 ftm")
+        expect(subject.parse("2 fathom 12 foot 144 inch").to_s).to eq("6 ftm")
+        expect(subject.parse("2 fathoms 12 feet 144 inches").to_s).to eq("6 ftm")
+      end
+
+      it "parses furlong, chain, and foot" do
+        expect(subject.parse("5 fur 40 ch 660 ft").to_s).to eq("10 fur")
+        expect(subject.parse("5 fur 40 ch 660 '").to_s).to eq("10 fur")
+        expect(subject.parse("5 furlong 40 chain 660 foot").to_s).to eq("10 fur")
+        expect(subject.parse("5 furlongs 40 chains 660 feet").to_s).to eq("10 fur")
+      end
+
+      it "parses foot, inch, and millimetre" do
+        expect(subject.parse("3 ft 6 in 90 mm").to_s).to eq("3.795275590551181 ft")
+        expect(subject.parse("3 ' 6 \" 90 mm").to_s).to eq("3.795275590551181 ft")
+        expect(subject.parse("3 foot 6 inch 90 millimeter").to_s).to eq("3.795275590551181 ft")
+        expect(subject.parse("3 feet 6 inches 90 millimeters").to_s).to eq("3.795275590551181 ft")
+        expect(subject.parse("3 foot 6 inch 90 millimetre").to_s).to eq("3.795275590551181 ft")
+        expect(subject.parse("3 feet 6 inches 90 millimetres").to_s).to eq("3.795275590551181 ft")
+      end
+
+      it "parses kilometre, metre, and millimetre" do
+        expect(subject.parse("9 km 500 m 3 mm").to_s).to eq("9.500003 km")
+        expect(subject.parse("9 kilometer 500 meter 3 millimeter").to_s).to eq("9.500003 km")
+        expect(subject.parse("9 kilometers 500 meters 3 millimeters").to_s).to eq("9.500003 km")
+        expect(subject.parse("9 kilometre 500 metre 3 millimetre").to_s).to eq("9.500003 km")
+        expect(subject.parse("9 kilometres 500 metres 3 millimetres").to_s).to eq("9.500003 km")
+      end
     end
 
     context "when invalid string is passed" do
@@ -83,6 +134,30 @@ RSpec.describe Unitify::CompositeMeasurements::Length do
         expect { subject.parse("9 feets 6 inche 50 centimeterss") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("9 foots 6 inched 50 centimetrez") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("9 fts 6 ines centimetrez") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("1 chs 66 fts 792 ins") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("1 chainss 66 feets 792 inchess") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("1 chainz 66 feetz 792 inchez") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("3 fts 6 ins 90 mms") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("3 feetss 6 inchess 90 millimeterss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("3 feetz 6 inchez 90 millimeterz") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("2 ftms 12 fts 144 ins") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("2 fathomss 12 feets 144 inchess") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("2 fathomz 12 feetz 144 inchez") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("5 furs 40 chs 660 fts") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("5 furlongss 40 chainss 660 feets") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("5 furlongz 40 chainz 660 feetz") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("9 kms 500 ms 3 mms") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("9 kilometress 500 metress 3 millimetress") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("9 kilometrez 500 metrez 3 millimetrez") }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("12 mis 3 furs 300 yds") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("12 miless 3 furlongss 300 yardss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("12 milez 3 furlongz 300 yardz") }.to raise_error(Unitify::ParseError)
       end
     end
   end
