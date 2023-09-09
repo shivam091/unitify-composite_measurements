096cf36e450412b5dacc78775cc6b39056a33ac5
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-09-09 18:39:57 +0530

    Bump version to `0.2.1`

--- spec/unitify/composite_measurements_spec.rb
+++ spec/unitify/composite_measurements_spec.rb
@@ -4,6 +4,6 @@
 
 RSpec.describe Unitify::CompositeMeasurements do
   it "has a valid version number" do
-    expect(Unitify::CompositeMeasurements::VERSION).to eq("0.2.0")
+    expect(Unitify::CompositeMeasurements::VERSION).to eq("0.2.1")
   end
 end
