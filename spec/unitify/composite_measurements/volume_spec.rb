5d4dac3eca6c1c73ef1930bb798a37523429891a
Harshal LADHE <harshal.ladhe.1@gmail.com>
2023-09-09 18:37:55 +0530

    Added new measurements parsing capabilities in `volume`

--- spec/unitify/composite_measurements/volume_spec.rb
+++ spec/unitify/composite_measurements/volume_spec.rb
@@ -17,6 +17,17 @@ RSpec.describe Unitify::CompositeMeasurements::Volume do
         expect(subject.parse("2 litre 250 millilitre").to_s).to eq("2.25 l")
         expect(subject.parse("2 litres 250 millilitres").to_s).to eq("2.25 l")
       end
+
+      it "parses cubic metre and cubic centimetre" do
+        expect(subject.parse("0.5 m³ 1000 cm³").to_s).to eq("0.501 m³")
+        expect(subject.parse("0.5 m3 1000 cm3").to_s).to eq("0.501 m³")
+        expect(subject.parse("0.5 m^3 1000 cm^3").to_s).to eq("0.501 m³")
+        expect(subject.parse("0.5 m*m*m 1000 cm*cm*cm").to_s).to eq("0.501 m³")
+        expect(subject.parse("0.5 cubic meter 1000 cubic centimeter").to_s).to eq("0.501 m³")
+        expect(subject.parse("0.5 cubic meters 1000 cubic centimeters").to_s).to eq("0.501 m³")
+        expect(subject.parse("0.5 cubic metre 1000 cubic centimetre").to_s).to eq("0.501 m³")
+        expect(subject.parse("0.5 cubic metres 1000 cubic centimetres").to_s).to eq("0.501 m³")
+      end
     end
 
     context "when invalid string is passed" do
@@ -25,6 +36,12 @@ RSpec.describe Unitify::CompositeMeasurements::Volume do
         expect { subject.parse("2 literss 250 milliliterr") }.to raise_error(Unitify::ParseError)
         expect { subject.parse("2 litress 250 millilitress").to_s }.to raise_error(Unitify::ParseError)
         expect { subject.parse("2 ls 250 mls").to_s }.to raise_error(Unitify::ParseError)
+
+        expect { subject.parse("0.5 ms³ 1000 cms³") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("0.5 cubic meterss 1000 cubic centimeterss") }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("0.5 cubic meterz 1000 cubic centimeterz").to_s }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("0.5 cubic metress 1000 cubic centimetress").to_s }.to raise_error(Unitify::ParseError)
+        expect { subject.parse("0.5 cubic metrez 1000 cubic centimetrez").to_s }.to raise_error(Unitify::ParseError)
       end
     end
   end
